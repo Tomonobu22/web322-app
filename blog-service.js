4d5a5401f6c19533298c0457f2eac89ddb36a70c
Tomonobu <fukchr@gmail.com>
2022-06-03 09:50:02 -0400

    Blog, post and categories working

---
+++ blog-service.js
@@ -0,0 +1,60 @@
+const fs = require("fs"); // required at the top of your module
+let posts = []
+let categories = []
+let published = []
+
+function intialize() {   
+    return new Promise((resolve, reject) => {
+        fs.readFile('./data/posts.json', 'utf8', (err, data) => {
+            if (err) reject("unable to read file")
+            else {
+                //console.log(data)
+                posts = JSON.parse(data)
+            }
+        })
+        fs.readFile('./data/categories.json', 'utf8', (err, data) => {
+            if (err) reject("unable to read file")
+            else{
+                //console.log(data)
+                categories = JSON.parse(data)
+            }
+        })
+        resolve("Reading process - SUCCESS!")
+    })
+}
+
+function getAllPosts() {
+    return new Promise((resolve, reject) => {
+        if(posts.length > 0) resolve(posts)
+        else reject('No results returned')
+    })
+}
+
+function getPublishedPosts() {
+    return new Promise((resolve, reject) => {
+        if(posts.length > 0) {
+            for(let i = 0; i < posts.length; i++){
+                if(posts[i].published)
+                published.push(posts[i])
+            }
+            resolve(published)
+        }
+        else reject('No results returned')
+    })  
+}
+
+function getCategories() {
+    return new Promise((resolve, reject) => {
+        if(categories.length > 0) resolve(categories)
+        else reject('No results returned')
+    })  
+}
+
+module.exports = {intialize,getAllPosts,getPublishedPosts,getCategories}
+
+// intialize().then(data => {
+//     console.log(data)
+// })
+// .catch(err => {
+//     console.log('Error:', error.message, ' error status code', err.status)
+// })
---
+++ server.js
@@ -0,0 +1,75 @@
+/*********************************************************************************
+* WEB322 – Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Name: Tomonobu Christian Fukuhara Tengan Student ID: 123475212 Date: May 30, 2022
+*
+* Online (Heroku) URL: https://afternoon-fortress-15422.herokuapp.com/
+*
+********************************************************************************/
+var blog = require('./blog-service')
+var express = require('express')
+var app = express()
+var path = require("path");
+const { getEnabledCategories } = require('trace_events');
+
+var PORT = process.eventNames.PORT || 8080
+app.use(express.static('public'))
+
+app.get('/',(req, res) => { 
+    //console.log(req)
+    res.redirect("/about")
+})
+
+app.get('/about',(req, res) => {
+    //console.log(req)
+    res.sendFile(path.join(__dirname,"/views/about.html"))
+})
+
+app.get('/blog',(req, res) => {
+    //console.log(req)
+    blog.getPublishedPosts().then((data) => {
+        res.send(data)
+    }).catch((err) => {
+        res.send(err)
+    })
+})
+
+app.get('/posts',(req, res) => {
+    //console.log(req)
+    blog.getAllPosts().then((data) => {
+        res.send(data)
+    }).catch((err) => {
+        res.send(err)
+    })
+})
+
+app.get('/categories',(req, res) => {
+    //console.log(req)
+    blog.getCategories().then((data) => {
+        res.send(data)
+    }).catch((err) => {
+        res.send(err)
+    })
+})
+
+app.get('/error', (req, res) => {
+    res.send('<h1>Page not found</h1><a href="/">Return Home Page</a>')    
+}) 
+
+app.use((req, res) => {
+    res.status(404).redirect('/error')
+})
+
+blog.intialize().then(()=> {
+    app.listen(PORT, () => {
+        console.log(`Express http server listening on port ${PORT}`)
+    })
+}).catch(()=>{
+    console.log('No results returned');
+})
+
+
+
