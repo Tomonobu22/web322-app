e4d0211e6337acebcf585ce14fdbf5376e1b1e44
Tomonobu <fukchr@gmail.com>
2022-06-16 07:33:04 -0400

    develop addPost and filters

--- blog-service.js
+++ blog-service.js
@@ -2,6 +2,8 @@ const fs = require("fs"); // required at the top of your module
 let posts = []
 let categories = []
 let published = []
+let posts_cat = []
+let posts_dt = []
 
 function intialize() {   
     return new Promise((resolve, reject) => {
@@ -51,7 +53,64 @@ function getCategories() {
     })  
 }
 
-module.exports = {intialize,getAllPosts,getPublishedPosts,getCategories}
+function addPost(postData) {
+    return new Promise((resolve, reject) => {
+        if(postData.published != false && postData.published != true) {
+            postData.published = false;
+        } else postData.published = true;
+        postData.id = posts.length + 1;
+        posts[posts.length] = postData;
+        resolve(postData)
+    }) 
+}
+
+function getPostsByCategory(category){
+    return new Promise((resolve, reject) => {
+        if(posts.length > 0) {
+            posts_cat = []
+            for(let i = 0; i < posts.length; i++){
+                if(posts[i].category == category)
+                posts_cat.push(posts[i])
+            }
+        }
+        if(posts_cat.length > 0) resolve(posts_cat)
+        else reject('No results returned')
+    })  
+}
+
+function getPostsByMinDate(minDateStr){
+    return new Promise((resolve, reject) => {
+        if(posts.length > 0) {
+            posts_dt = []
+            for(let i = 0; i < posts.length; i++){
+                if(new Date(posts[i].postDate) >= new Date(minDateStr))
+                posts_dt.push(posts[i])
+            }
+        }
+        if(posts_dt.length > 0) resolve(posts_dt)
+        else reject('No results returned')
+    })  
+}
+
+function getPostById(id){
+    return new Promise((resolve, reject) => {
+        var flag = 0
+        let post_id
+        if(posts.length > 0) {
+            flag = 0
+            for(let i = 0; i < posts.length; i++){
+                if(posts[i].id == id){
+                    post_id = posts[i]
+                    flag = 1
+                }
+            }
+        }
+        if(flag = 1) resolve(post_id)
+        else reject('No results returned')
+    })  
+}
+
+module.exports = {intialize,getAllPosts,getPublishedPosts,getCategories, addPost, getPostsByCategory, getPostsByMinDate, getPostById}
 
 // intialize().then(data => {
 //     console.log(data)
--- server.js
+++ server.js
@@ -1,18 +1,30 @@
 /*********************************************************************************
-* WEB322 – Assignment 2
+* WEB322 – Assignment 3
 * I declare that this assignment is my own work in accordance with Seneca Academic Policy.
 * No part of this assignment has been copied manually or electronically from any other source
 * (including web sites) or distributed to other students.
 *
-* Name: Tomonobu Christian Fukuhara Tengan Student ID: 123475212 Date: Jun 03, 2022
+* Name: Tomonobu Christian Fukuhara Tengan Student ID: 123475212 Date: Jun 16, 2022
 *
-* Online (Heroku) URL: https://guarded-inlet-93508.herokuapp.com/
+* Online (Heroku) URL: 
 *
 ********************************************************************************/
 var blog = require('./blog-service')
 var express = require('express')
 var app = express()
-var path = require("path");
+var path = require("path")
+const multer = require("multer")
+const cloudinary = require('cloudinary').v2
+const streamifier = require('streamifier')
+
+cloudinary.config({
+    cloud_name: 'dhvyi6zkx',
+    api_key: '637167755885338',
+    api_secret: 'tbbZuv_5mM3NDEW78YeafyZymlc',
+    secure: true
+});
+
+const upload = multer()
 const { getEnabledCategories } = require('trace_events');
 
 var PORT = process.env.PORT || 8080
@@ -39,7 +51,35 @@ app.get('/blog',(req, res) => {
 
 app.get('/posts',(req, res) => {
     //console.log(req)
-    blog.getAllPosts().then((data) => {
+    var category = req.query.category;
+    var minDateStr = req.query.minDate;
+
+    if(category > 0 && category < 6){
+        blog.getPostsByCategory(category).then((data) => {
+            res.send(data)
+        }).catch((err) => {
+            res.send({message: err})
+        })
+    }
+    else if(minDateStr != null){
+        blog.getPostsByMinDate(minDateStr).then((data) => {
+            res.send(data)
+        }).catch((err) => {
+            res.send({message: err})
+        })
+    }
+    else {
+        blog.getAllPosts().then((data) => {
+            res.send(data)
+        }).catch((err) => {
+            res.send({message: err})
+        })
+    }
+})
+
+app.use('/posts/:id', (req, res, next) => {
+    var id = req.params.id;
+    blog.getPostById(id).then((data) => {
         res.send(data)
     }).catch((err) => {
         res.send({message: err})
@@ -47,7 +87,6 @@ app.get('/posts',(req, res) => {
 })
 
 app.get('/categories',(req, res) => {
-    //console.log(req)
     blog.getCategories().then((data) => {
         res.send(data)
     }).catch((err) => {
@@ -55,13 +94,51 @@ app.get('/categories',(req, res) => {
     })
 })
 
+app.get('/posts/add',(req, res) => {
+    res.sendFile(path.join(__dirname,"/views/addPost.html"))
+})
+
 app.get('/error', (req, res) => {
     res.send('<h1>Page not found</h1><a href="/">Return Home Page</a>')    
 }) 
 
+app.post('/posts/add', upload.single('featureImage'), (req, res) => {
+    let streamUpload = (req) => {
+        return new Promise((resolve, reject) => {
+            let stream = cloudinary.uploader.upload_stream(
+            (error, result) => {
+                if (result) {
+                    resolve(result);
+                } else {
+                    reject(error);
+                }
+            }
+        );
+        streamifier.createReadStream(req.file.buffer).pipe(stream);
+        });
+    };
+
+    async function upload(req) {
+        let result = await streamUpload(req);
+        console.log(result);
+        return result;
+    }
+    upload(req).then((uploaded)=> {
+        req.body.featureImage = uploaded.url;
+        blog.addPost(req.body).then(() => {
+            res.redirect('/posts')
+        }).catch(()=>{
+            console.log('No results returned');
+        })
+    // TODO: Process the req.body and add it as a new Blog Post before redirecting to/posts
+    })
+})
+
 app.use((req, res) => {
     res.status(404).redirect('/error')
 })
+
+
 
 blog.intialize().then(()=> {
     app.listen(PORT, () => {
